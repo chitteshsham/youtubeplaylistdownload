6d0cb81fbaf158229d6a87d469b6e467961b79ee
Chittesh Sham <40590101+chitteshsham@users.noreply.github.com>
2018-07-06 15:18:14 +0400

    Update ytdownloader.sh

--- ytdownloader.sh
+++ ytdownloader.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-oldcount=$(ls ./playlist/ -1 | wc -l) ;
+
+echo "paste your youtube playlist url"
+read ytplaylist
+
+echo "insert folder name to download"
+read folder
+
+mkdir $folder
+oldcount=$(ls ./$folder/ -1 | wc -l) ;
 echo "number of songs already downloaded = $oldcunt";
-newcount=$(youtube-dl --flat-playlist --dump-single-json "https://www.youtube.com/watch?v=Bm9rz-llMhE&list=PLrXFitO_y5jUAsNkAzI6_K0kXAeZ1io5v" | jq '.entries' | grep "ie_key" | wc -l) ;
+newcount=$(youtube-dl --flat-playlist --dump-single-json $ytplaylist | jq '.entries' | grep "ie_key" | wc -l) ;
 echo "number of songs in Youtube Playlist= $newcount";
 
-if [ $newcount -gt $oldcount ]; then  youtube-dl --extract-audio --audio-format mp3 -o "./playlist/%(title)s.%(ext)s" "https://www.youtube.com/watch?v=Bm9rz-llMhE&list=PLrXFitO_y5jUAsNkAzI6_K0kXAeZ1io5v"; echo "done" ; else echo "already upto date"; fi ;
+if [ $newcount -gt $oldcount ]; then youtube-dl --extract-audio --audio-format mp3 -o "./playlist/%(title)s.%(ext)s" $ytplaylist; echo "done" ; else echo "already upto date"; fi ;
