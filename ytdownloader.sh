4da13ff0997405b0d1d1f4097a42a909c9fed389
Chittesh Sham <40590101+chitteshsham@users.noreply.github.com>
2018-07-24 14:37:18 +0400

    Update ytdownloader.sh

--- ytdownloader.sh
+++ ytdownloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-oldcunt=$(ls ./playlist/ -1 | wc -l) ;
+oldcunt=$(ls ./playlistah/ -1 | wc -l) ;
 echo "number of songs already downloaded = $oldcunt";
 newcunt=$(youtube-dl --flat-playlist --dump-single-json "https://www.youtube.com/watch?v=Bm9rz-llMhE&list=PLrXFitO_y5jUAsNkAzI6_K0kXAeZ1io5v" | jq '.entries' | grep "ie_key" | wc -l) ;
 echo "number of songs in Youtube Playlist= $newcunt";
